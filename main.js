34581369f921d7f33f351d551fb22c02d9e2d168
Олександр Радчук <73553177+brolo1313@users.noreply.github.com>
2021-12-06 20:49:14 +0200

    access properties through square brackets

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let user = {
     name: 'John',
-    age:30
+    age:30,
+    'two word' : true
 }
 
 user.isAdmin = true;
@@ -9,3 +10,4 @@ delete user.name
 console.log(user);
 console.log(user.name);
 console.log(user.isAdmin);
+console.log(user["two word"]);
