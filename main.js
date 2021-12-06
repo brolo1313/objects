b90f4e0b328384c2d58f473c77510280c01e211f
Олександр Радчук <73553177+brolo1313@users.noreply.github.com>
2021-12-06 21:16:28 +0200

    receive value  in prompt

--- main.js
+++ main.js
@@ -4,10 +4,19 @@ let user = {
     'two word' : true
 }
 
+let key = 'prompt("What y wont?", "age")'
+
 user.isAdmin = true;
 delete user.name
 
-console.log(user);
-console.log(user.name);
-console.log(user.isAdmin);
-console.log(user["two word"]);
+console.log(user[key]);
+// console.log(user.name);
+// console.log(user.isAdmin);
+// console.log(user["two word"]);
+
+let fruit = prompt("Какой фрукт купить?", "apple");
+
+let bag = {
+  [fruit]: 5,
+};
+console.log( bag.apple ); // 
