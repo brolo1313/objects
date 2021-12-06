97692642440a287ee2eb0059d54e5b656d978d9f
Олександр Радчук <73553177+brolo1313@users.noreply.github.com>
2021-12-06 22:21:25 +0200

    task for practic

--- main.js
+++ main.js
@@ -35,9 +35,68 @@ const David = makeUser( 'David', 30, 'Rom')
 
 let user2 = { name: "John", age: 30, city: 'Kiev' };
 
-for ( key in user2){
-    console.log(user2[key]);
-}
+// for ( key in user2){
+//     console.log(user2[key]);
+// }
 
 // console.log("age" in user2);
 // console.log("city" in user2); 
+
+let info = { 
+    name : 'js',
+    surnmae : 'ECMAScript',
+
+}
+info.name = 'c++'
+delete info.name
+// console.log(info.name);
+
+let shedule = {};
+
+function isEmpty(obj){
+    for(let key in obj) {
+        if(key) {
+            console.log(false);
+        }else {
+            return true
+        }
+    }
+}
+// isEmpty(shedule);
+
+const target = {
+    house: 'first'
+}
+target.house = 'second'
+// console.log(target.house);
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+
+let sum = 0;
+for (let key in salaries){
+     sum += salaries[key];
+    
+}
+// console.log(sum);
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+ function multiplyNumeric(obj){
+     for (let key in obj){
+         console.log(typeof(obj[key]));
+         if(typeof(obj[key]) == 'number' ){
+             obj[key] *= 2
+             console.log(menu);
+         }
+     }
+ }
+
+//  multiplyNumeric(menu)
